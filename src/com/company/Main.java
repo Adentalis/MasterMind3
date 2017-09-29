339577adee2443dc17a6a02f2fdb232aff6644b6
daniel <daniel.n96@t-online.de>
2017-09-29 12:35:53 +0200

    added Hello Daniel

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hallo Daniel");
     }
 }
+
+// immer zuerst changes commiten unten im version control
